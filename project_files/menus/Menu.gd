1bd5988a6aca5c78dfbd3a9fd051af88be456274
IceCobe <jaalaba@gmail.com>
2024-04-29 10:54:32 -0700

    file restructure

--- project_files/menus/Menu.gd
+++ project_files/menus/Menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file('res://main_room.tscn')
+	get_tree().change_scene_to_file('res://levels/main_room.tscn')
 
 
 func _on_quit_pressed():
